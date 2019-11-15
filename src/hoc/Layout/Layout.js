3f9acae5f8bdd9047e2dcea0d92eb25811e840df
excavator <str_token@mail.ru>
2019-11-15 16:42:36 +0300

    add dinfmic className scc

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import classes from './Layout.css';
 
 class Layout extends React.Component {
   render(){
     return (
-      <div>
-
+      <div className={classes.Layout}>
         <main>
           { this.props.children }
         </main>
