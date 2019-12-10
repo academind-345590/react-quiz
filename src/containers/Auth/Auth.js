587545d8fe42d1d0f5cf397a934535228138508f
excavator <str_token@mail.ru>
2019-12-10 16:43:07 +0300

    add authorization in Frebase

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import classes from './Auth.css';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
 import is from 'is_js';
+import axios from 'axios';
 
 export default class Auth extends React.Component {
 
@@ -36,12 +37,37 @@ export default class Auth extends React.Component {
     }
   }
 
-  loginHandler =() => {
+  loginHandler =async () => {
+    const keyUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCe4VSiz14XAl4_7ItsboTpbNTQWm7MLFM';
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
 
+    try{
+      const response = await axios.post(keyUrl, authData);
+      console.log(response.data);
+    }catch(e){
+      console.log(e);
+    }
   }
 
-  registerHandler=() => {
+  registerHandler=async () => {
+    const keyUrl = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCe4VSiz14XAl4_7ItsboTpbNTQWm7MLFM';
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.password.value,
+      returnSecureToken: true
+    }
 
+    try{
+      const response = await axios.post(keyUrl, authData);
+      console.log(response.data);
+    }catch(e){
+      console.log(e);
+    }
+    
   }
 
   submitHandler= event => {
