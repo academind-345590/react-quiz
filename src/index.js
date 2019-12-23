65b108296bc5bd657401d9bfddf3561e29dffec0
excavator <str_token@mail.ru>
2019-12-23 11:57:31 +0300

    add redux

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from './store/reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+// from https://github.com/zalmoxisus/redux-devtools-extension
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+    }) : compose;
+
+const store = createStore(
+    rootReducer, 
+    composeEnhancers(
+      applyMiddleware(thunk)
+    )
+  );
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>, document.getElementById('root'));
 serviceWorker.unregister();
