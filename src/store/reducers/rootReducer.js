6ff4186c8fb849e724b98573afbd5e4a258672e0
excavator <str_token@mail.ru>
2019-12-25 17:49:10 +0300

    add redux to authorization

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import quizReduser from './quiz';
 import createReduser from './create';
+import authReduser from './auth';
 
 export default combineReducers({
   quiz: quizReduser,
-  create: createReduser
+  create: createReduser,
+  auth: authReduser
 })
