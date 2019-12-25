c8011cbe1cd289f1dc3277caaf08f9f524608a8b
excavator <str_token@mail.ru>
2019-12-25 20:08:56 +0300

    add redux to menu items

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,6 +2,7 @@ import React from 'react';
 import classes from './Layout.css';
 import MenuToggle from '../../components/Navigation/MenuToggle/MenuToggle';
 import Drawer from '../../components/Navigation/Drawer/Drawer';
+import { connect } from 'react-redux';
 
 class Layout extends React.Component {
   state = {
@@ -28,6 +29,7 @@ class Layout extends React.Component {
         <Drawer 
           isOpen = {this.state.menu}
           onClose = {this.menuCloseHandler}
+          isAuthenticated={this.props.isAuthenticated}
         />
 
         <MenuToggle 
@@ -43,4 +45,10 @@ class Layout extends React.Component {
   }
 }
 
-export default Layout
+function mapStateToProps(state){
+  return{
+    isAuthenticated: !!state.auth.token
+  }
+}
+
+export default connect(mapStateToProps)(Layout)
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -14,7 +14,7 @@ export default function auth(email, password, isLogin){
     const response = await axios.post(url, authData); 
     const data = response.data;
 
-    const expirationDate = new Date(new Date().getTime() + data.expiresIn *1000)
+    const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
     
     localStorage.setItem('token', data.idToken );
     localStorage.setItem('userId', data.localId );
@@ -39,6 +39,23 @@ export function logout(){
   localStorage.removeItem('expirationDate');
   return {
     type: AUTH_LOGOUT
+  }
+}
+
+export function autoLogin(){
+  return dispatch => {
+    const token = localStorage.getItem('token');
+    if(!token){
+      dispatch(logout())
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate'));
+      if (expirationDate <= new Date()){
+        dispatch(logout())
+      } else {
+        dispatch(authSuccess(token))
+        dispatch(autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000 ))
+      }
+    }
   }
 }
 
